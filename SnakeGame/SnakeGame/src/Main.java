761cfa77b49e7842bbfdd4c6119c974af26cc847
sir <sir@UCEBNA-NB-06.gjkt.local>
2018-11-26 15:37:59 +0100

    obrázky teď

--- SnakeGame/SnakeGame/src/Main.java
+++ SnakeGame/SnakeGame/src/Main.java
@@ -1,5 +1,8 @@
 
 public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 	//zkouška naèítání
 	public static void main(String[] args) {
 		System.out.println("Nìco");
