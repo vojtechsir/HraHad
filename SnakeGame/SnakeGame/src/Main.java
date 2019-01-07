702fabcc52ab8743c5b16c8d6b04c87506a8c286
sir <sir@UCEBNA-NB-06.gjkt.local>
2019-01-07 15:10:17 +0100

    nová main

--- SnakeGame/SnakeGame/src/Main.java
+++ SnakeGame/SnakeGame/src/Main.java
@@ -1,17 +1,23 @@
+
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class Main {
-	
-	public Main() {
-		
-		JFrame frame = new JFrame();
-		
-	}
-	
+
 	public static void main(String[] args) {
-	
-	     new Main();
-	     
+		JFrame obj = new JFrame();
+		Gameplay gameplay = new Gameplay();
+		
+		obj.setBounds(10, 10, 905, 700);
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+		
+		
+
 	}
 
 }
